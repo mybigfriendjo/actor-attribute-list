d20f5161d079a0b9f5961056ca38c07e545c1ee5
mybigfriendjo <mybigfriendjo@users.noreply.github.com>
2024-03-10 22:46:01 +0100

    working state

--- scripts/5e-actor-attributes.js
+++ scripts/5e-actor-attributes.js
@@ -1,13 +1,88 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
+const MODULE_NAME = "5e-actor-attributes";
 
-Hooks.on("init", function () {
-  console.log(
-    "This code runs once the Foundry VTT software begins its initialization workflow."
-  );
-});
+export class AttributeViewer extends Application {
+  static characterName = "";
 
-Hooks.on("ready", function () {
-  console.log(
-    "This code runs once core initialization is ready and game data is available."
-  );
+  constructor(actor) {
+    super();
+    this.actor = actor;
+    this.characterName = actor.name;
+  }
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      id: "attribute-viewer",
+      template: "modules/" + MODULE_NAME + "/templates/AttributeViewer.hbs",
+      popOut: true,
+      minimizable: true,
+      resizable: true,
+      editable: false,
+      height: 600,
+      title:
+        game.i18n.localize("attributeViewer.appTitle") +
+        " " +
+        this.characterName,
+    });
+  }
+
+  _buildAttributeStrings(currentAttribute, previousString) {
+    let attributes = [];
+    const type = typeof currentAttribute;
+    if (type === "object") {
+      for (const key in currentAttribute) {
+        const str = previousString + "." + key;
+        attributes = attributes.concat(
+          this._buildAttributeStrings(currentAttribute[key], str)
+        );
+      }
+    } else if (type === "string") {
+      attributes.push({
+        attName: previousString,
+        attValue:
+          currentAttribute.length < 50
+            ? currentAttribute
+            : currentAttribute.substring(0, 50) + "...",
+      });
+    } else if (type === "boolean" || type === "number" || type === "bigint") {
+      attributes.push({
+        attName: previousString,
+        attValue: currentAttribute.toString(),
+      });
+    }
+    return attributes;
+  }
+
+  _getAttributeCategories(rollData) {
+    const categories = [];
+    for (const key in rollData) {
+      categories.push({
+        categoryName: key,
+        attributes: this._buildAttributeStrings(rollData[key], key),
+      });
+    }
+    return categories;
+  }
+
+  getData() {
+    const data = {
+      actorName: this.actor.name,
+      categories: this._getAttributeCategories(this.actor.getRollData()),
+    };
+    return data;
+  }
+}
+
+Hooks.on("getActorSheet5eCharacterHeaderButtons", (app, buttons) => {
+  if (!game.user.isGM) {
+    return;
+  }
+  buttons.unshift({
+    class: "showAttributes",
+    icon: "fas fa-dice",
+    label: game.i18n.localize("actorSheet.headerButtonLabel"),
+    onclick: async () => {
+      let actor = app.actor;
+      new AttributeViewer(actor).render(true);
+    },
+  });
 });
