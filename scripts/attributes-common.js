6c44639403beff2cc47c5f6e0fadfa38acc61891
mybigfriendjo <mybigfriendjo@users.noreply.github.com>
2024-03-19 01:48:28 +0100

    forgot to propagate maxDepth; formatting

--- scripts/attributes-common.js
+++ scripts/attributes-common.js
@@ -6,30 +6,30 @@
  * @returns Array of key value pairs of the attributes
  */
 export function generateAttributes(currentAttribute, previousString, currentDepth, maxDepth) {
-    if (currentDepth > maxDepth) {
-      return [];
-    }
-  
-    let attributes = [];
-    const type = typeof currentAttribute;
-    if (type === "object") {
-      for (const key in currentAttribute) {
-        const newString = previousString + "." + key;
-        attributes = attributes.concat(
-          generateAttributes(currentAttribute[key], newString, currentDepth + 1)
-        );
-      }
-    } else if (type === "string") {
-      attributes.push({
-        attName: previousString,
-        attValue:
-          currentAttribute.length < 50 ? currentAttribute : currentAttribute.substring(0, 50) + "...",
-      });
-    } else if (type === "boolean" || type === "number" || type === "bigint") {
-      attributes.push({
-        attName: previousString,
-        attValue: currentAttribute.toString(),
-      });
-    }
-    return attributes;
+  if (currentDepth > maxDepth) {
+    return [];
   }
+
+  let attributes = [];
+  const type = typeof currentAttribute;
+  if (type === "object") {
+    for (const key in currentAttribute) {
+      const newString = previousString + "." + key;
+      attributes = attributes.concat(
+        generateAttributes(currentAttribute[key], newString, currentDepth + 1, maxDepth)
+      );
+    }
+  } else if (type === "string") {
+    attributes.push({
+      attName: previousString,
+      attValue:
+        currentAttribute.length < 50 ? currentAttribute : currentAttribute.substring(0, 50) + "...",
+    });
+  } else if (type === "boolean" || type === "number" || type === "bigint") {
+    attributes.push({
+      attName: previousString,
+      attValue: currentAttribute.toString(),
+    });
+  }
+  return attributes;
+}
